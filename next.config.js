20d6f18c56330ea1e855e89e51ae2aaa8f93bf2c
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-06-30 01:07:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = {
     domains: ["res.cloudinary.com", "media.dev.to"],
   },
   env: {
-    customKey: "my-value",
     EMAILJS_SERVICE_ID: "service_h2zywgh",
     EMAILJS_TEMPLATE_ID: "template_jt7ho06",
     EMAILJS_PUBLIC_KEY: "O9gfw24UaIQVHt0Yw",
