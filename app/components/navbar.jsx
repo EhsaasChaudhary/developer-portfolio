a453d58b2f90aa58b57badf1d5ae9140b6422067
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-06-30 02:24:14 +0530

    navbar sticky in layout

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -29,7 +29,7 @@ const pages = [
 
 function Navbar() {
   return (
-    <nav className=" fixed bg-transparent">
+    <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
           <Link
