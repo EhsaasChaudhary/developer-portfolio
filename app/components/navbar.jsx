50b3be6a8d630a5d97a3e23e73081783b73abbc2
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-06-30 02:26:46 +0530

    fixed new

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -30,7 +30,7 @@ const pages = [
 function Navbar() {
   return (
     <nav className="bg-transparent">
-      <div className="flex items-center justify-between py-5">
+      <div className=" fixed flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
           <Link
             href="/"
